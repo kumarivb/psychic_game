5a0810705cc7110298fb70b164dfc1f2638fd614
kvb <kvb@github.com>
2017-12-01 15:30:47 -0600

    added steps I think I need to take to game.js to create the psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+	/*
+	Steps:
+	-determine how many guesses user gets.
+	-computer to randomly choose a letter.
+	-capture computers choice.
+	-user to guess a letter.
+	-capture users guess.
+	-compare computers choice to users guess and see if they match or not.
+	-determine a win or a loss.
+	-display the result (win,loss).
+	-display guesses left.
+	-display letters guessed thus far.
+	-display win or lose when number of guesses has been reached. 
+	*/
