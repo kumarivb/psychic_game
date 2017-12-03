38c32ad8800315b2952fb83ea024625ff713a450
kvb <kvb@github.com>
2017-12-03 16:39:16 -0600

    linked game.js to index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,37 @@
-	/*
-	Steps:
-	-determine how many guesses user gets.
-	-computer to randomly choose a letter.
-	-capture computers choice.
-	-user to guess a letter.
-	-capture users guess.
-	-compare computers choice to users guess and see if they match or not.
-	-determine a win or a loss.
-	-display the result (win,loss).
-	-display guesses left.
-	-display letters guessed thus far.
-	-display win or lose when number of guesses has been reached. 
-	*/
+	//	make sure Javascript doesn't run until the HTML is finished loading
+		$(document).ready(function() {
+
+	//	determine how many guesses user gets
+
+
+	//  computer to randomly choose a letter
+
+
+	//	capture computers choice
+
+
+	//	user to guess a letter
+
+
+	//	capture users guess
+
+
+	//	compare computers choice to users guess and see if they match or not
+
+
+	//	determine a win or a loss
+
+
+	//	display the result (win,loss)
+
+
+	//	display guesses left
+
+
+	//	display letters guessed thus far
+
+
+	//	display win or lose when number of guesses has been reached
+	
+
+		});
