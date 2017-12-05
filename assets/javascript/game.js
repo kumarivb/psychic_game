4c72d9ac76c930dc7bc3051d4d129100bb4bfcfd
kvb <kvb@github.com>
2017-12-05 12:29:02 -0600

    added javascript to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,57 @@
-	//	make sure Javascript doesn't run until the HTML is finished loading
-		$(document).ready(function() {
+//	make sure Javascript doesn't run until the HTML is finished loading
+	$(document).ready(function() {
 
-	//	determine how many guesses user gets
-		var numOfGuesses = 15;
+//	determine how many guesses user gets
+	var guessesLeft = 15;
 
-	// 	track number of guesses, wins, and losses
-		var guessCounter = 0;
-		var winCounter = 0;
-		var lossCounter = 0;
+// 	track number of wins, losses, and guesses so far
+	var winCounter = 0;
+	var lossCounter = 0;
+	var guessesSoFar = [];
 
-	//  computer to randomly choose a letter, letter choices
-		var randomLetter = "";
-		var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//  computer random letter choices
+	var randLetterChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-	//	capture computers choice
+//	start capturing
+	document.onkeyup = function(event) {
+
+//	capture computers choice
+	var compLetterChoice = randLetterChoice[Math.floor(Math.random() * randLetterChoice.length)];
+
+//	capture users guess
+	var userGuess = String.fromCharccode(event.keyCode).toLowerCase();
+
+//	display user guesses
+	guessesSoFar.push(userGuess);
+
+//	compare computers choice to users guess and see if they match or not, winner, reset
+	if (userGuess == compLetterChoice) {
+		wins++;
+		alert('You Won!');
+		guessesLeft = 15;
+		guessesSoFar.lenth = 0;
+	}
+
+//	no guesses left
+	else if (guessesLeft == 0) {
+		
+	}
 
 
-	//	user to guess a letter
+//	compare computers choice to users guess and see if they match or not, loser
+
+//	determine a win or a loss
 
 
-	//	capture users guess
+//	display the result (win,loss)
 
 
-	//	compare computers choice to users guess and see if they match or not
+//	display letters guessed thus far
 
 
-	//	determine a win or a loss
+//	display win or lose when number of guesses has been reached
 
 
-	//	display the result (win,loss)
+		};
 
-
-	//	display guesses left
-
-
-	//	display letters guessed thus far
-
-
-	//	display win or lose when number of guesses has been reached
-	
-
-		});
+	});
