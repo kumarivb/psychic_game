4d282153ad50d2212cbc8d93b06b6bb71af3831a
kvb <kvb@github.com>
2017-12-05 13:17:23 -0600

    added more javascript to game.js and changed ID tag on index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,42 +13,45 @@
 	var randLetterChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //	start capturing
-	document.onkeyup = function(event) {
+	document.onkeypress = function(event) {
 
 //	capture computers choice
 	var compLetterChoice = randLetterChoice[Math.floor(Math.random() * randLetterChoice.length)];
 
 //	capture users guess
-	var userGuess = String.fromCharccode(event.keyCode).toLowerCase();
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 //	display user guesses
 	guessesSoFar.push(userGuess);
 
 //	compare computers choice to users guess and see if they match or not, winner, reset
 	if (userGuess == compLetterChoice) {
-		wins++;
+		winCounter++;
 		alert('You Won!');
 		guessesLeft = 15;
-		guessesSoFar.lenth = 0;
+		guessesSoFar.length = 0;
 	}
 
 //	no guesses left
 	else if (guessesLeft == 0) {
-		
+		lossCounter++;
+		alert('You ran out of guesses. You lose.')
+		guessesLeft = 15;
+		guessesSoFar.length = 0;
 	}
 
+// 	lower number of guesses left
+	else if (userGuess !== compLetterChoice) {
+		guessesLeft--;
+	}
+
+// 	display results in html
+	document.getElementById("wins").innerHTML = + wins 
 
 //	compare computers choice to users guess and see if they match or not, loser
-
 //	determine a win or a loss
-
-
 //	display the result (win,loss)
-
-
 //	display letters guessed thus far
-
-
 //	display win or lose when number of guesses has been reached
 
 
