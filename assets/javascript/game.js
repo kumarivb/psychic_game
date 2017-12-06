f859fb0d1ec5e97072a6643d18aff60204be8330
kvb <kvb@github.com>
2017-12-06 12:31:25 -0600

    added some basic styling to style.css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -52,7 +52,7 @@
 	document.getElementById("loses").innerHTML = lossCounter;
 	document.getElementById("guessLeft").innerHTML = guessesLeft;
 	document.getElementById("guessSoFar").innerHTML = guessesSoFar;
-	
+
 //	compare computers choice to users guess and see if they match or not, loser
 //	determine a win or a loss
 //	display the result (win,loss)
